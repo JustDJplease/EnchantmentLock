88f667821d506b09fcaf8551afdd7daa8dbe324a
TheBlockBender <floris.jolink@gmail.com>
2018-09-28 14:18:58 +0200

    imported code from old project

--- src/main/java/me/theblockbender/enchantmentlock/listeners/AnvilListener.java
+++ src/main/java/me/theblockbender/enchantmentlock/listeners/AnvilListener.java
@@ -1,4 +1,77 @@
 package me.theblockbender.enchantmentlock.listeners;
 
-public class AnvilListener {
+import me.theblockbender.enchantmentlock.EnchantmentLock;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.AnvilInventory;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryView;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.List;
+
+public class AnvilListener implements Listener {
+
+    private EnchantmentLock main;
+
+    /**
+     * Constructor
+     *
+     * @param main Instance of the main class.
+     */
+    public AnvilListener(EnchantmentLock main) {
+        this.main = main;
+    }
+
+    /**
+     * Anvil event.
+     *
+     * @param event Event called.
+     */
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        HumanEntity human = event.getWhoClicked();
+        if (!(human instanceof Player)) return;
+
+        Player player = (Player) human;
+        Inventory inventory = event.getClickedInventory();
+        if (!(inventory instanceof AnvilInventory)) return;
+
+        InventoryView inventoryView = event.getView();
+        int slot = event.getRawSlot();
+        if (slot != inventoryView.convertSlot(slot)) return;
+        if (slot != 2) return;
+
+        ItemStack item = inventory.getItem(0);
+        ItemStack mergedWith = inventory.getItem(1);
+        ItemStack result = inventory.getItem(2);
+        boolean isRepair = false;
+
+        if (item == null) return;
+        if (result == null) return;
+        if (!item.hasItemMeta()) return;
+        ItemMeta meta = item.getItemMeta();
+        if (!meta.hasLore()) return;
+
+        if (mergedWith != null) {
+            if (mergedWith.getType() != Material.ENCHANTED_BOOK) isRepair = true;
+            if (main.getConfig().getBoolean("BlockMergeEnchants") && (item.getEnchantments() != result.getEnchantments()))
+                isRepair = false;
+        }
+
+        if (!isRepair) {
+            List<String> lore = meta.getLore();
+            for (String loreline : lore) {
+                if (!main.identifiers.contains(loreline)) continue;
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', main.getConfig().getString("Message")));
+                event.setCancelled(true);
+            }
+        }
+    }
 }
--- src/main/java/me/theblockbender/enchantmentlock/listeners/EnchantmentTableListener.java
+++ src/main/java/me/theblockbender/enchantmentlock/listeners/EnchantmentTableListener.java
@@ -1,4 +1,55 @@
 package me.theblockbender.enchantmentlock.listeners;
 
-public class EnchantmentTableListener {
+import me.theblockbender.enchantmentlock.EnchantmentLock;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.List;
+import java.util.UUID;
+
+public class EnchantmentTableListener implements Listener {
+
+    private EnchantmentLock main;
+//    private List<UUID> cancelDoubleSending = new ArrayList<>();
+
+    /**
+     * Constructor
+     *
+     * @param main Instance of the main class.
+     */
+    public EnchantmentTableListener(EnchantmentLock main) {
+        this.main = main;
+    }
+
+    /**
+     * Enchanting Table event.
+     *
+     * @param event Event called.
+     */
+    @EventHandler
+    public void onEnchant(PrepareItemEnchantEvent event) {
+        ItemStack item = event.getItem();
+        Player player = event.getEnchanter();
+        final UUID uuid = player.getUniqueId();
+
+        if (!item.hasItemMeta()) return;
+        ItemMeta meta = item.getItemMeta();
+        if (!meta.hasLore()) return;
+        List<String> lore = meta.getLore();
+
+        for (String loreline : lore) {
+            if (!main.identifiers.contains(loreline)) continue;
+//            if (!cancelDoubleSending.contains(uuid)) {
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', main.getConfig().getString("Message")));
+//                cancelDoubleSending.add(uuid);
+//                main.getServer().getScheduler().runTaskLater(main, (Runnable) () -> cancelDoubleSending.remove(uuid), 2L);
+//            }
+            event.setCancelled(true);
+        }
+    }
 }
