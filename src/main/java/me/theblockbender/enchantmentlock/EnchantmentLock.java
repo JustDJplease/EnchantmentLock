fe5d0d987b42fb1d6bc7aed1d8ce3ba3a360f4d4
TheBlockBender <floris.jolink@gmail.com>
2018-09-28 13:39:05 +0200

    init commit of version 2.0

---
+++ src/main/java/me/theblockbender/enchantmentlock/EnchantmentLock.java
@@ -0,0 +1,4 @@
+package me.theblockbender.enchantmentlock;
+
+public class EnchantmentLock {
+}
---
+++ src/main/java/me/theblockbender/enchantmentlock/listeners/AnvilListener.java
@@ -0,0 +1,4 @@
+package me.theblockbender.enchantmentlock.listeners;
+
+public class AnvilListener {
+}
